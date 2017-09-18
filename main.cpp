bea1253eef6c7599bf1a97094070db283be5f42f
Malthe <malthef@gmail.com>
2017-09-18 18:11:40 +0200

    need to terminate the while loop

--- main.cpp
+++ main.cpp
@@ -13,13 +13,44 @@
 
 using namespace std;
 
-
+void printVectors(vector<int> &a, vector<int> &b);
 
 int main() {
     //field playground[n][m];
 
     //week2 week2;
     //week2.primeFactorization();
+
+    vector<int> a;
+    vector<int> b;
+
+    char c;
+    int d;
+    
+    while(cin >> c) {
+
+
+        cin >> d;
+        if(c== 'a') {
+            a.push_back(d);
+        } else {
+            b.push_back(d);
+        }
+    }
+    printVectors(a, b);
+    int x = 1;
+
+    return 0;
+
+}
+
+void printVectors(vector<int> &a, vector<int> &b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    for(int i = 0; i <a.size(); i++) {
+        cout << a[i];
+    }
 
 }
 
