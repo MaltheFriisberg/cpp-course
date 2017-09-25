b44d104f3d665e2170d2d098fb4234f6e8d6729d
Malthe <malthef@gmail.com>
2017-09-25 19:20:21 +0200

    almost there

--- main.cpp
+++ main.cpp
@@ -86,12 +86,48 @@ public:
 };
 
 int main() {
-    fraction fraction1(1,3);
-    fraction1.display();
+    //fraction fraction1(1,3);
+    //fraction1.display();
     //fraction1.add(fraction1);
-    fraction fraction2(2,7);
-    fraction1.div(fraction2);
-    fraction1.display();
+    //fraction fraction2(2,7);
+    //fraction1.div(fraction2);
+    //fraction1.display();
+
+    int numerator, numerator1;
+    int divisor, divisor1;
+    string symbol;
+    string operation;
+    //bool numeratorRead = false;
+    //getline(cin, line);
+
+    /*for(int i = 0; i <line.size(); i++) {
+        char c = line[i];
+        if(isdigit(c) &&!numeratorRead) {
+            numerator = (int)c;
+            numeratorRead = true;
+        } else if(isdigit(c) &&numeratorRead) {
+            divisor = (int)c;
+        }
+    }*/
+    //cin >> numerator >> symbol >> divisor >> operation >> numerator1 >> symbol >> divisor1;
+    for(int i = 0; i <3; i ++) {
+        cin >> numerator >> symbol >> divisor >> operation >> numerator1 >> symbol >> divisor1;
+        fraction fraction1(numerator, divisor);
+        fraction fraction2(numerator1,divisor1);
+
+        if(operation.compare("+") == 0) {
+            fraction1.add(fraction2);
+            fraction1.display();
+        } else if(operation.compare("*")==0) {
+            fraction1.mult(fraction2);
+            fraction1.display();
+        } else if(operation.compare("div")==0) {
+            fraction1.div(fraction2);
+            fraction1.display();
+        }
+        cout << "" <<endl;
+    }
+
 
 
 
